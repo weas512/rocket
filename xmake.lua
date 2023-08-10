5a6f1214bd2a8fd5d4252674753db17f618d9e82
weas512 <noto.mella@skiff.com>
2023-08-11 01:41:41 +0300

    initial commit

---
+++ xmake.lua
@@ -0,0 +1,27 @@
+set_project("rocket")
+set_version("0.0.1")
+
+add_rules("mode.debug", "mode.release")
+add_requires("sfml")
+
+target("rocket")
+    -- Global preferences
+    set_kind("binary")
+    set_languages("c++17")
+    
+    add_files("src/*.cpp") 
+    add_files("src/tests/*.cpp")
+     
+    add_packages("sfml")
+
+    -- Debug preferences
+    -- none, faster, fastest, smallest
+    if is_mode("debug") then
+        set_optimize("none")
+    else
+        set_optimize("fastest")
+    end
+
+
+-- To set platform, toolchain, archetecture or xmake mode use that command:
+-- > xmake f --toolchain=msvc -p windows -a x64 -m debug
